a0b820029e82c5a2a99912fd70381fae9a0152cd
jjwu724 <54125440+jjwu724@users.noreply.github.com>
2025-01-14 18:12:30 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,12 +17,37 @@ fetch('./shared.html')
             link.setAttribute('aria-current', 'page');
         }
     });
+    const templateContact = templateDoc.querySelector('template#contact').content
+    document.getElementById('contact-shared').appendChild(templateContact.cloneNode(true));
     const computedStyle = getComputedStyle(document.documentElement);
-    const areaCode = computedStyle.getPropertyValue('--area-code');
-    const phoneNumber = computedStyle.getPropertyValue('--phone-number');
+    const areaCode = computedStyle.getPropertyValue('--area-code').slice(1, -1);
+    const phoneNumber = computedStyle.getPropertyValue('--phone-number').slice(1, -1);
     const fullPhoneNumber = areaCode + phoneNumber;
-    document.getElementById('phoneLink').href = "tel:" + fullPhoneNumber;
-    document.getElementById('areaCodeDisplay').textContent = areaCode;
-    document.getElementById('phoneNumberDisplay').textContent = phoneNumber;
+    const address1 = computedStyle.getPropertyValue('--address-1').slice(1, -1);
+    const address2 = computedStyle.getPropertyValue('--address-2').slice(1, -1);
+    const webUrl = computedStyle.getPropertyValue('--webUrl').slice(1, -1);
+    let i;
+    let l;
+    const phoneLinks = document.getElementsByClassName('phoneLink');
+    l = phoneLinks.length;
+    for (i=0; i<l; i++) {phoneLinks[i].href = "tel:" + fullPhoneNumber;}
+    const areaCodeSpans = document.getElementsByClassName('areaCode');
+    l = areaCodeSpans.length;
+    for (i=0; i<l; i++) {areaCodeSpans[i].textContent = areaCode;}
+    const phoneNumberSpans = document.getElementsByClassName('phoneNumber');
+    l = phoneNumberSpans.length;
+    for (i=0; i<l; i++) {phoneNumberSpans[i].textContent = phoneNumber;}
+    const address1Spans = document.getElementsByClassName('address-1');
+    l = address1Spans.length;
+    for (i=0; i<l; i++) {address1Spans[i].textContent = address1;}
+    const address2Spans = document.getElementsByClassName('address-2');
+    l = address2Spans.length;
+    for (i=0; i<l; i++) {address2Spans[i].textContent = address2;}
+    const webUrlSpans = document.getElementsByClassName('webUrl');
+    l = webUrlSpans.length;
+    for (i=0; i<l; i++) {webUrlSpans[i].textContent = webUrl;}
+    const webUrlLinks = document.getElementsByClassName('webUrlLink');
+    l = webUrlLinks.length;
+    for (i=0; i<l; i++) {webUrlLinks[i].href = `https://${webUrl}`;}
   })
   .catch(error => console.error('Error loading template:', error));
