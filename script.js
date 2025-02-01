9602994427171fb2e0171ca1db459c5d3d5d2993
jjwu724 <54125440+jjwu724@users.noreply.github.com>
2025-02-01 10:38:03 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ fetch('./shared.html')
     document.head.appendChild(templateHead.cloneNode(true));
     const templateHeader = templateDoc.querySelector('template#header').content;
     document.getElementById('header-shared').appendChild(templateHeader.cloneNode(true));
-    const templateNav = templateDoc.querySelector('template#nav').content;
-    document.getElementById('nav-shared').appendChild(templateNav.cloneNode(true));
+    const templateNav1 = templateDoc.querySelector('template#nav1').content;
+    document.getElementById('nav-shared1').appendChild(templateNav1.cloneNode(true));
+    const templateNav2 = templateDoc.querySelector('template#nav2').content;
+    document.getElementById('nav-shared2').appendChild(templateNav2.cloneNode(true));
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => {
@@ -15,6 +17,7 @@ fetch('./shared.html')
         if (href === currentPage) {
             link.classList.add('active');
             link.setAttribute('aria-current', 'page');
+            link.parentElement.classList.add('no-pip');
         }
     });
     const templateContact = templateDoc.querySelector('template#contact').content
@@ -56,5 +59,37 @@ fetch('./shared.html')
     const webUrlLinks = document.getElementsByClassName('webUrlLink');
     l = webUrlLinks.length;
     for (i=0; i<l; i++) {webUrlLinks[i].href = `https://${webUrl}`;}
+    observeNav();
   })
   .catch(error => console.error('Error loading template:', error));
+
+function observeNav() {
+  const shrinkingSpacer = document.getElementById("shrinking-spacer");
+  const growingWrapper = document.getElementById("growing-wrapper");
+  const nav2 = document.getElementById("nav-shared2");
+  const ul1 = document.getElementById('ul1');
+  const ul2 = document.getElementById('ul2');
+  let wrapperMinWidth;
+  const observer = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const target = entry.target;
+      const width = entry.contentRect.width;
+      if (target === shrinkingSpacer && width === 0) {
+        observer.unobserve(shrinkingSpacer);
+        nav2.style.display = 'flex';
+        const allLi = ul1.querySelectorAll('li');
+        const liToMove = Array.from(allLi).slice(4);
+        liToMove.forEach(li => ul2.appendChild(li));
+        wrapperMinWidth = growingWrapper.offsetWidth + 1;
+        requestAnimationFrame(() => {observer.observe(growingWrapper);});
+      } else if (target === growingWrapper && width > wrapperMinWidth) {
+        observer.unobserve(growingWrapper);
+        nav2.style.display = 'none';
+        const allLi2 = ul2.querySelectorAll('li');
+        allLi2.forEach(li => ul1.appendChild(li));
+        requestAnimationFrame(() => {observer.observe(shrinkingSpacer);});
+      }
+    }
+  });
+  observer.observe(shrinkingSpacer);
+}
