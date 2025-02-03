116188452fe1296bb9749bebc37b3681559c0ebd
jjwu724 <54125440+jjwu724@users.noreply.github.com>
2025-02-03 07:12:43 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ fetch('./shared.html')
     document.head.appendChild(templateHead.cloneNode(true));
     const templateHeader = templateDoc.querySelector('template#header').content;
     document.getElementById('header-shared').appendChild(templateHeader.cloneNode(true));
-    const templateNav1 = templateDoc.querySelector('template#nav1').content;
-    document.getElementById('nav-shared1').appendChild(templateNav1.cloneNode(true));
-    const templateNav2 = templateDoc.querySelector('template#nav2').content;
-    document.getElementById('nav-shared2').appendChild(templateNav2.cloneNode(true));
+    const templateNav = templateDoc.querySelector('template#nav').content;
+    document.getElementById('nav-container-shared').appendChild(templateNav.cloneNode(true));
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => {
@@ -64,32 +62,46 @@ fetch('./shared.html')
   .catch(error => console.error('Error loading template:', error));
 
 function observeNav() {
-  const shrinkingSpacer = document.getElementById("shrinking-spacer");
-  const growingWrapper = document.getElementById("growing-wrapper");
+  const decoration = document.getElementById("nav-decoration");
+  const spacer = document.getElementById("shrinking-spacer");
+  const nav1 = document.getElementById("nav-shared1");
   const nav2 = document.getElementById("nav-shared2");
   const ul1 = document.getElementById('ul1');
   const ul2 = document.getElementById('ul2');
-  let wrapperMinWidth;
+  let nav1MinWidth;
   const observer = new ResizeObserver(entries => {
     for (let entry of entries) {
       const target = entry.target;
       const width = entry.contentRect.width;
-      if (target === shrinkingSpacer && width === 0) {
-        observer.unobserve(shrinkingSpacer);
-        nav2.style.display = 'flex';
-        const allLi = ul1.querySelectorAll('li');
-        const liToMove = Array.from(allLi).slice(4);
+      if (target === spacer && width === 0) {
+        observer.unobserve(spacer);
+        nav1MinWidth = nav1.offsetWidth + 1;
+        const allLi1 = ul1.querySelectorAll('li');
+        const liToMove = Array.from(allLi1).slice(4);
         liToMove.forEach(li => ul2.appendChild(li));
-        wrapperMinWidth = growingWrapper.offsetWidth + 1;
-        requestAnimationFrame(() => {observer.observe(growingWrapper);});
-      } else if (target === growingWrapper && width > wrapperMinWidth) {
-        observer.unobserve(growingWrapper);
-        nav2.style.display = 'none';
+        nav1.className = 'nav1-background-gradient';
+        decoration.style.display = 'block';
+        nav2.style.display = 'flex';
+        requestAnimationFrame(() => {observer.observe(nav1);});
+      } else if (target === nav1 && width > nav1MinWidth) {
+        observer.unobserve(nav1);
+        nav1.className = 'nav1-background-solid';
+        nav2.style.display = decoration.style.display = 'none';
         const allLi2 = ul2.querySelectorAll('li');
         allLi2.forEach(li => ul1.appendChild(li));
-        requestAnimationFrame(() => {observer.observe(shrinkingSpacer);});
+        requestAnimationFrame(() => {observer.observe(spacer);});
+      } else if (target === nav1) {
+        const nav1Rect = nav1.getBoundingClientRect();
+        const nav2Rect = nav2.getBoundingClientRect();
+        Object.assign(decoration.style, {
+          top: `${nav2Rect.top + window.scrollY}px`,
+          left: `${nav2Rect.left + window.scrollX}px`
+        });
+        const decorationRot = Math.atan2((nav1Rect.top - nav2Rect.top), nav2Rect.width);
+        decoration.style.transform = `rotate(${decorationRot}rad)`;
+        decoration.style.height = `${nav1Rect.height / Math.cos(decorationRot)}px`;
       }
     }
   });
-  observer.observe(shrinkingSpacer);
+  observer.observe(spacer);
 }
